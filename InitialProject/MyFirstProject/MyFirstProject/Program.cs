6a0b3355cf802df4e600e83dd043934107fa9b77
dinhujr <c.souza0804@gmail.com>
2015-10-25 01:20:31 -0200

    Add comments in the class

--- InitialProject/MyFirstProject/MyFirstProject/Program.cs
+++ InitialProject/MyFirstProject/MyFirstProject/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace MyFirstProject
 {
+    /// <summary>
+    /// This is the generally class for any shape
+    /// </summary>
     public abstract class Shape
     {
         public double width { get; set; }
         public double height { get; set; }
+        /// <summary>
+        /// This method call the calculation Area Method
+        /// </summary>
         public void showArea()
         {
             Console.WriteLine("Area: {0} with the following Dimensions\n\twidth: {1} \n\tand \n\theight: {2}", this.calculateArea(), this.width, this.height);
@@ -17,6 +23,9 @@ namespace MyFirstProject
         public abstract double calculateArea();
     }
 
+    /// <summary>
+    /// this class implements Shape as a Rectangle
+    /// </summary>
     class Rectangle : Shape
     {
         
@@ -34,6 +43,9 @@ namespace MyFirstProject
 
     }
 
+    /// <summary>
+    /// This class implements Shape as a Square
+    /// </summary>
     public class Square : Shape
     {
         public Square(double side)
@@ -41,6 +53,10 @@ namespace MyFirstProject
             this.height = this.width = side;
         }
 
+        /// <summary>
+        /// this method calculates the square area.
+        /// </summary>
+        /// <returns></returns>
         public override double calculateArea()
         {
             double area = width * height;
@@ -48,6 +64,9 @@ namespace MyFirstProject
         }
     }
 
+    /// <summary>
+    /// this is the executable class
+    /// </summary>
     public class ExecutionRectangle
     {
         static void Main(string[] args)
